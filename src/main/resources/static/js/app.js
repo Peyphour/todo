7fb885ca6341f1429340623868e365131e3b6031
Bertrand Nancy <bertrand@slimpay.com>
2017-11-22 14:42:52 +0100

    Add create project and visualisation

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,11 +1,20 @@
 var APP = {
     projectLoadStatus: [],
+    projects: new Map(),
+    currentProject: {},
     processTemplate: function (container, template_name, data) {
         $('#' + container).html(Handlebars.compile($('#' + template_name).html())(data))
     },
     init: function() {
         window.onhashchange = APP.dispatch
         APP.dispatch()
+
+        Handlebars.registerHelper("equals", function(a, b, opts) {
+            if(a === b) // Or === depending on your needs
+                return opts.fn(this);
+            else
+                return opts.inverse(this);
+        })
     },
     dispatch: function() {
         switch(location.hash) {
@@ -16,11 +25,25 @@ var APP = {
             case '#projects':
                 API.getProjects(APP.projectsLoaded)
                 break;
+            case '#create-project':
+                APP.processTemplate('root', 'new-project-template')
+        }
+        if(location.hash.startsWith("#project_-_")) {
+            var projectName = decodeURI(location.hash.split('_-_')[1])
+            console.log(projectName)
+            APP.currentProject = APP.projects.get(projectName)
+            API.getProjectTodos(APP.currentProject, function(body) {
+                APP.processTemplate('root', 'project-template', {
+                    projectName: projectName,
+                    todos: body._embedded.todos
+                })
+            })
         }
     },
     projectsLoaded: function(projects) {
         for(var i = 0; i < projects._embedded.projects.length; i++) {
             APP.projectLoadStatus[i] = false;
+            APP.projects.set(projects._embedded.projects[i].title, projects._embedded.projects[i]);
             (function (i) {
                 return API.getProjectTodos(projects._embedded.projects[i], function(todos) {
                     projects._embedded.projects[i].todos = todos._embedded.todos
@@ -36,6 +59,13 @@ var APP = {
             if(APP.projectLoadStatus[i] === false)
                 return
         }
-        APP.processTemplate('root', 'project-template', projects._embedded.projects)
+        APP.processTemplate('root', 'project-list-template', projects._embedded.projects)
+    },
+    newProject: function() {
+        API.addProject({
+            title: $("#new-project-name").val()
+        }, function() {
+            $("#new-project-feedback").text("New project created!")
+        })
     }
 }
